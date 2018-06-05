136e3e970786f0db898ea116bee84fe6014fb865
Pritish Sai <Pritish.sai@gmail.com>
2018-06-05 19:33:32 -0400

    edited major portions of wmt project

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, length: {maximum: 200}, format: { with: VALID_EMAIL_REGEX }
-	validates :description, length: { minimum: 10, maximum: 500 }
+	validates :email, presence: :true, length: {maximum: 200}, format: { with: VALID_EMAIL_REGEX }
+	validates :description, presence: :true, length: { minimum: 10, maximum: 500 }
 end
